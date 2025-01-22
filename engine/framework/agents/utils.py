96f032a509aed558c2e69272acd662cb76b057d5
MANOJ MANIVANNAN <23746043+manojmanivannan@users.noreply.github.com>
2025-01-22 15:16:08 +0000

    fix chat history messages

--- engine/framework/agents/utils.py
+++ engine/framework/agents/utils.py
@@ -21,9 +21,9 @@ class Utils:
             # log.debug(f"DEBUG chat history Message: {message} | class:{type(message)} | msg.type: {message.type} | attrs: {dir(message)}")
             message_content = message.content
             if message.type == "human":
-                messages.append(f'Human: {message_content}')
+                messages.append(('human',message_content))
             elif message.type == "ai":
-                messages.append(f'AI: {message_content}')
+                messages.append(('assistant', message_content))
             else:
                 messages.append(f'{message_content}') #BaseMessage(content=message_content))
                 
